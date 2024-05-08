e16cefc12167b889929a8193a76a29959f8a54fb
Hermano Uchoa <hermano.uchoa@icc.org.br>
2024-05-08 07:35:13 -0300

    Declaração de ArrayList

---
+++ lang/java/ArrayList_Declaracao.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Hermano Uchoa - https://github.com/hermanouchoa
+ */
+public class ArrayList_Declaracao {
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        
+        ArrayList<String> nomes = new ArrayList<>();
+        nomes.add("Jesus");
+        nomes.add("Maria");
+        nomes.add("José");
+        
+        System.out.println("\nLista de Nomes:");
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+        
+    }    
+}
