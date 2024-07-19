ecbf2f25515778ae37b98a1c5cf912b4a5c98f3b
Hermano Uchôa <hermanouchoa@gmail.com>
2024-07-18 22:40:49 -0300

    tratando para adicionar coluna de classe

--- dataanalytics/python/analiseexploratoriodedados/fase1/tech_challenge/producaoCSV.py
+++ dataanalytics/python/analiseexploratoriodedados/fase1/tech_challenge/producaoCSV.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
 # Caminho para o arquivo CSV
-#file_path = 'C:\\projetos\\wisdombox\\dataanalytics\\python\\analiseexploratoriodedados\\fase1\\tech_challenge\\dadosbrutos\\Producao.csv'
-#new_file_path = 'C:\\projetos\\wisdombox\\dataanalytics\\python\\analiseexploratoriodedados\\fase1\\tech_challenge\\dadosbrutos\\'
-
-file_path = 'C:\\projetos\\hermanouchoa\\wisdombox\\dataanalytics\\python\\analiseexploratoriodedados\\fase1\\tech_challenge\\dadosbrutos\\Producao.csv'
-new_file_path = 'C:\\projetos\\hermanouchoa\\wisdombox\\dataanalytics\\python\\analiseexploratoriodedados\\fase1\\tech_challenge\\dados\\'
+file_path = 'C:\\projetos\\wisdombox\\dataanalytics\\python\\analiseexploratoriodedados\\fase1\\tech_challenge\\dadosbrutos\\Producao.csv'
+new_file_path = 'C:\\projetos\\wisdombox\\dataanalytics\\python\\analiseexploratoriodedados\\fase1\\tech_challenge\\dadosbrutos\\'
 
 # Carregar o arquivo CSV sem cabeçalho, usando ";" como delimitador
 df = pd.read_csv(file_path, header=None, delimiter=';')
@@ -20,8 +17,29 @@ header = ['index', 'classificacao','nome_classificacao'] + [str(year) for year i
 # Adicionar o cabeçalho ao dataframe
 df.columns = header
 
+# Adicionar a nova coluna "classe" na segunda posição
+def classify(row):
+    classificacao = row['classificacao']
+    if classificacao.startswith('vm'):
+        return 'VINHO DE MESA'
+    elif classificacao.startswith('vv'):
+        return 'VINHO FINO DE MESA (VINÍFERA)'
+    elif classificacao.startswith('su'):
+        return 'SUCO'
+    elif classificacao.startswith('de'):
+        return 'DERIVADOS'
+    else:
+        return 'OUTRO'
+
+df['classe'] = df.apply(classify, axis=1)
+
+# Reorganizar as colunas para que "classe" esteja na segunda posição
+cols = df.columns.tolist()
+cols.insert(1, cols.pop(cols.index('classe')))
+df = df[cols]
+
 # Salvar o dataframe com o novo cabeçalho
-output_path = new_file_path+'Producao_ok.csv'
+output_path = new_file_path + 'Producao_ok.csv'
 df.to_csv(output_path, index=False, sep=';')
 
 print(f'Arquivo salvo como {output_path}')
