01c945cde2947b5c39dca8d54d34e356416e21c0
Hermano Uchôa <hermanouchoa@gmail.com>
2024-07-19 00:11:28 -0300

    Função para geração da base de dados final

---
+++ dataanalytics/python/analiseexploratoriodedados/fase1/tech_challenge/exportacaoFinal.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import glob
+import os
+
+# Diretório onde os arquivos CSV estão localizados
+file_path = 'C:\\projetos\\wisdombox\\dataanalytics\\python\\analiseexploratoriodedados\\fase1\\tech_challenge\\dados\\'
+
+# Padrão para encontrar os arquivos CSV que começam com "Exp"
+padrao_arquivos = os.path.join(file_path, 'Exp*.csv')
+
+# Lista para armazenar os novos dados
+todos_dados = []
+
+# Encontra todos os arquivos que correspondem ao padrão
+arquivos_csv = glob.glob(padrao_arquivos)
+
+# Itera sobre cada arquivo CSV encontrado
+for arquivo_csv in arquivos_csv:
+    print(f'Lendo o arquivo {arquivo_csv}')
+    
+    # Lê o arquivo CSV
+    df = pd.read_csv(arquivo_csv, delimiter=';')
+    
+    # Itera sobre cada linha do dataframe
+    for index, row in df.iterrows():
+        classe = row['classe']
+        pais = row['pais']
+        
+        # Itera sobre cada ano no dataframe
+        for ano in range(1970, 2020):
+            valor_col = f'valor_{ano}'
+            kg_col = f'kg_{ano}'
+            
+            # Verifica se as colunas existem no dataframe
+            if valor_col in df.columns and kg_col in df.columns:
+                valor = row[valor_col]
+                kg = row[kg_col]
+                
+                # Adiciona a nova linha de dados na lista
+                todos_dados.append([classe, pais, ano, kg, valor])
+
+# Cria um novo dataframe com os dados transformados
+novo_df = pd.DataFrame(todos_dados, columns=['classe', 'pais', 'ano', 'kg', 'valor'])
+
+# Salva o novo dataframe em um arquivo CSV
+novo_df.to_csv(file_path+'ExpotacaoFinal.csv', index=False, sep=';')
