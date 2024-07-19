2ec4b7d87be1aee5d15eede60a1a6081f8479c5c
Hermano Uchoa <hermano.uchoa@icc.org.br>
2024-07-19 16:13:29 -0300

    tratamento de dados de processamento

--- dataanalytics/python/analiseexploratoriodedados/fase1/tech_challenge/processamentoCSV.py
+++ dataanalytics/python/analiseexploratoriodedados/fase1/tech_challenge/processamentoCSV.py
@@ -11,6 +11,14 @@ file_path = os.getenv('PATH_DADOS_BRUTOS')
 new_file_path = os.getenv('PATH_DADOS')
 
 
+# Mapeamento de arquivos para a classe
+mapeamento_classe = {
+    "ProcessaAmericanas.csv": "Americanas e hibridas",
+    "ProcessaMesa.csv": "Uvas de mesa",
+    "ProcessaSemclass.csv": "Sem classificação",
+    "ProcessaViniferas.csv": "Viniferas"
+}
+
 arquivos_com_prefixo = []
 for arquivo in os.listdir(file_path):
     if arquivo.startswith("Processa") and arquivo.endswith('.csv'):
@@ -31,8 +39,29 @@ for arquivo in os.listdir(file_path):
         # Adicionar o cabeçalho ao dataframe
         df.columns = header
 
+        # Adicionar a nova coluna "classe" na segunda posição
+        def tipo(row):
+            classificacao = row['classificacao']
+            if classificacao.startswith('ti_'):
+                return 'TINTAS'
+            elif classificacao.startswith('br_'):
+                return 'BRANCAS E ROSADAS'
+            else:
+                return None
+
+        df['tipo'] = df.apply(tipo, axis=1)
+        # Remover linhas onde a coluna "classe" é None
+        df = df.dropna(subset=['tipo'])
+
+        # Adicionar a coluna 'classe' com base no nome do arquivo
+        df['classe'] = mapeamento_classe.get(arquivo, 'Desconhecido')
+
+        # Reorganizar as colunas para que 'classe' seja a segunda
+        cols = ['index', 'classe', 'tipo','classificacao', 'nome_classificacao'] + header[5:]
+        df = df[cols]        
+
         # Salvar o dataframe com o novo cabeçalho
-        output_path = new_file_path+arquivo+'_ok.csv'
+        output_path = new_file_path+arquivo
         df.to_csv(output_path, index=False, sep=';')
 
         print(f'Arquivo salvo como {output_path}')
---
+++ dataanalytics/python/analiseexploratoriodedados/fase1/tech_challenge/processamentoFinal.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import glob
+import os
+
+from dotenv import load_dotenv # pip install python-dotenv
+
+# Carrega as variáveis do arquivo .env para o ambiente
+load_dotenv()
+
+# Diretório onde os arquivos CSV estão localizados
+#file_path = 'C:\\projetos\\wisdombox\\dataanalytics\\python\\analiseexploratoriodedados\\fase1\\tech_challenge\\dados\\'
+file_path = os.getenv('PATH_DADOS')
+
+# Padrão para encontrar os arquivos CSV que começam com "Exp"
+padrao_arquivos = os.path.join(file_path, 'Processa*.csv')
+
+# Lista para armazenar os novos dados
+todos_dados = []
+
+# Encontra todos os arquivos que correspondem ao padrão
+arquivos_csv = glob.glob(padrao_arquivos)
+
+# Itera sobre cada arquivo CSV encontrado
+for arquivo_csv in arquivos_csv:
+    print(f'Lendo o arquivo {arquivo_csv}')
+    
+    # Lê o arquivo CSV
+    df = pd.read_csv(arquivo_csv, delimiter=';')
+    
+    # Itera sobre cada linha do dataframe
+    for index, row in df.iterrows():
+        
+        classe = row['classe']
+        tipo = row['tipo']
+        classificacao = row['classificacao']
+        nome_classificacao = row['nome_classificacao']
+        
+        # Itera sobre cada ano no dataframe
+        for ano in range(1970, 2020):
+            kg_col = f'{ano}'
+            print(ano)
+            
+            # Verifica se as colunas existem no dataframe
+            if kg_col in df.columns:
+                kg = row[kg_col]
+                
+                # Adiciona a nova linha de dados na lista
+                todos_dados.append([classe, tipo, classificacao, nome_classificacao, ano, kg])
+
+# Cria um novo dataframe com os dados transformados
+novo_df = pd.DataFrame(todos_dados, columns=['classe', 'tipo','classificacao','nome_classificacao', 'ano', 'kg'])
+
+# Salva o novo dataframe em um arquivo CSV
+novo_df.to_csv(file_path+'ProcessamentoFinal.csv', index=False, sep=';')
