4ec103adfeb7bf042a71a6760e00081b24fa551e
Hermano Uchôa <hermanouchoa@gmail.com>
2024-07-19 23:11:01 -0300

    ajuste de diretorio

--- dataanalytics/python/analiseexploratoriodedados/fase1/tech_challenge/paises.py
+++ dataanalytics/python/analiseexploratoriodedados/fase1/tech_challenge/paises.py
@@ -34,7 +34,7 @@ def salvar_paises_em_csv(paises, arquivo_saida):
     else:
         print("Nenhum país foi encontrado nos arquivos CSV fornecidos.")
 
-diretorio = os.getenv('PATH_DADOS')
+diretorio = os.getenv('PATH_DADOS')+"\\final\\"
 
 arquivo_saida = diretorio+'paises.csv'
 paises = listar_paises_unicos(diretorio)
